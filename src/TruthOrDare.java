759abf8589c4fc175f292b315946b201237a216d
matthew <you@example.com>
2019-10-14 17:52:58 -0700

    hagfha

--- src/TruthOrDare.java
+++ src/TruthOrDare.java
@@ -1,9 +1,14 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 import java.util.Scanner;
 
 public class TruthOrDare {
     private Scanner scanner = new Scanner(System.in);
+    String[] names;
+    HashMap<String, Integer> skips = new HashMap<>();
+
+    int previousCount = 0;
 
     private String lastNumber;
     private Object String;
@@ -14,14 +19,14 @@ public class TruthOrDare {
     public void playerSetup(){
         System.out.println("How many players do you have?");
         int playerAmount = scanner.nextInt();
+        names = new String[playerAmount];
         System.out.println("Enter the names: ");
-        String[] names = new String[playerAmount];
         for(int i = 0; i < names.length; i++){
             System.out.println("What is the name of player " + (i + 1));
             names[i] = scanner.next();
         }
         for(String test: names){
-            System.out.println(test);
+            skips.put(test, 0);
         }
         System.out.println("Are you ready?");
         String confirmation = scanner.next();
@@ -39,5 +44,28 @@ public class TruthOrDare {
             }else{
 
             }
+        System.out.println("Are you ready to continue");
+            String readyOrNot = scanner.nextLine();
+            if (readyOrNot.equalsIgnoreCase("yes")){
+                for(int i = 0; i <  names.length; i++){
+                    fiftyFifty();
+                }
+                System.out.println("Are you done playing?");
+                String answer = scanner.nextLine();
+                if(answer.equalsIgnoreCase("no")) {
+                    fiftyFifty();
+
+                }else{
+                    System.out.println("Thank you for playing.");
+                }
+            }
+            else if(readyOrNot.equalsIgnoreCase("skip")){
+
+//                skips.get();
+            }else if(readyOrNot.equalsIgnoreCase("No")){
+                System.out.println("Thanks for playing");
+            }
+
+
     }
 }
